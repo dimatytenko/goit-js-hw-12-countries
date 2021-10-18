9093bf38f102f21e5d0afc6f7932a6ffea991126
Dima Tytenko <amgpetronass@gmail.com>
2021-10-18 23:16:35 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+import getRefs from './js/getRefs';
+import countriesTemplate from './templates/countries.hbs';
+
+const refs = getRefs();
+
+function fetchCountries(searchQuery) {
+  return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+    .then(responce => responce.json())
+    .then(countries => {
+      // return countries;
+      appendCountriesMarkup(countries);
+    });
+}
+console.log(fetchCountries('ukraine'));
+
+function appendCountriesMarkup(countries) {
+  refs.cardContainer.innerHTML = countriesTemplate(countries);
+}
---
+++ src/js/getRefs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+  return {
+    cardContainer: document.querySelector('.js-country-container'),
+    input: document.querySelector('#searchQuery'),
+  }
+}
