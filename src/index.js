59d6f2f70aa9cace1b3ca3e3b423d2472085b994
Dima Tytenko <amgpetronass@gmail.com>
2021-10-19 21:04:16 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import './sass/main.scss';
 import getRefs from './js/getRefs';
 import countriesTemplate from './templates/countries.hbs';
+import listCountriesTemplate from './templates/list-countries.hbs';
+import fetchCountries from './js/fetchCountries';
+const debounce = require('lodash.debounce');
 
 const refs = getRefs();
+refs.input.addEventListener('input', debounce(onSearch, 500));
 
-function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
-    .then(responce => responce.json())
-    .then(countries => {
-      // return countries;
-      appendCountriesMarkup(countries);
-    });
+function onSearch(event) {
+  const query = event.target.value;
+  renderCountries(query);
 }
-console.log(fetchCountries('ukraine'));
+
+const BASE_URL = 'https://restcountries.com/v2';
+
+function renderCountries(searchQuery) {
+  fetchCountries(searchQuery).then(countries => {
+    if (countries.length === 1) {
+      appendCountriesMarkup(countries);
+    } else if (countries.length >= 2 && countries.length <= 10) {
+      appendListCountries(countries);
+    }
+  });
+}
 
 function appendCountriesMarkup(countries) {
   refs.cardContainer.innerHTML = countriesTemplate(countries);
+}
+
+function appendListCountries(countries) {
+  refs.cardContainer.innerHTML = listCountriesTemplate(countries);
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,9 @@
-fetchCountries(searchQuery);
+const BASE_URL = 'https://restcountries.com/v2';
+
+export default function fetchCountries(searchQuery) {
+  return fetch(`${BASE_URL}/name/${searchQuery}`)
+    .then(responce => responce.json())
+    .then(countries => {
+      return countries;
+    });
+}
