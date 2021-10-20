a3028a8e6289436d973ae539afe34e611b024035
Dima Tytenko <amgpetronass@gmail.com>
2021-10-20 18:38:32 +0300

    all

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import getRefs from './js/getRefs';
 import countriesTemplate from './templates/countries.hbs';
 import listCountriesTemplate from './templates/list-countries.hbs';
 import fetchCountries from './js/fetchCountries';
+
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
 const debounce = require('lodash.debounce');
 
 const refs = getRefs();
@@ -10,16 +15,22 @@ refs.input.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(event) {
   const query = event.target.value;
+  if (query === '') {
+    refs.cardContainer.innerHTML = '';
+    return;
+  }
   renderCountries(query);
 }
 
 function renderCountries(searchQuery) {
   fetchCountries(searchQuery).then(countries => {
-    if (countries.length === 1) {
+    if (countries.length > 10) {
+      tooManyMatches();
+    } else if (countries.length === 1) {
       appendCountriesMarkup(countries);
     } else if (countries.length >= 2 && countries.length <= 10) {
       appendListCountries(countries);
-    }
+    } else notFound();
   });
 }
 
@@ -29,4 +40,15 @@ function appendCountriesMarkup(countries) {
 
 function appendListCountries(countries) {
   refs.cardContainer.innerHTML = listCountriesTemplate(countries);
+}
+
+function tooManyMatches() {
+  error({
+    text: 'Too many matches found. Please enter a more specific query!',
+  });
+}
+function notFound() {
+  error({
+    text: 'Not Found!',
+  });
 }
